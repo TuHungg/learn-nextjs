6924238428a3622c3a016930d4f39281c3925ae1
TuHungg <tuhoanghaihung@gmail.com>
2022-04-14 16:39:48 +0700

    login/logout requests

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import httpProxy from 'http-proxy'
+import Cookies from 'cookies'
+
 // type Data = {
 //   name: string
 // }
@@ -13,22 +15,30 @@ export const config = {
 
 const proxy = httpProxy.createProxyServer()
 
-export default function handler( req: NextApiRequest, res: NextApiResponse<any>)
-{
-   //don't send cookie to api sever
-   req.headers.cookie = ''
+export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  return new Promise((resolve) => {
+    //convert cookies to hearder Authorziration
+    const cookies = new Cookies(req, res)
+    const accessToken = cookies.get('access_token')
+    if (accessToken) {
+      req.headers.authorization = `Bearer ${accessToken}`
+    }
 
-   // https://js-post-api.herokuapp.com/api/students/
-   // /api/students
+    //don't send cookie to api sever
+    req.headers.cookie = ''
 
+    // https://js-post-api.herokuapp.com/api/students/
+    // /api/students
 
-   proxy.web(req,res,{
-     target: process.env.API_URL,
-     changeOrigin: true,
-     selfHandleResponse: false,
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false,
+    })
 
-   })
-
-  // res.status(200).json({ name: 'Path - Match all here' })
+    proxy.once('proxyRes', () => {
+      resolve(true)
+    })
+    // res.status(200).json({ name: 'Path - Match all here' })
+  })
 }
-
